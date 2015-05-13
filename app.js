626b78881cba5773a2f86c5b1176c5baf713a03a
Priyanka Acharya <priyanka2172@gmail.com>
2015-05-13 10:36:47 -0700

    Updated with Fibonacci

---
+++ app.js
@@ -0,0 +1,14 @@
+/**
+ * Created by nisum-user on 5/13/15.
+ */
+function Fibonacci(){
+
+    this.recursive=function(n){
+        if(n<=2) {
+            return 1;
+        }
+        else{
+            return this.recursive(n-1)+ this.recursive(n-2);
+        }
+    };
+}
